dfc7c5150e1833fedb75600667a23a1fc620d0ae
David Bensoussan <minipada@gmail.com>
2017-10-19 13:59:24 +0200

    fix: Uses /proc/device-tree to get board information
    Using /proc/cpuinfo works for 32 bits but not for 64 bits. The fix used is mostly copy pasting Cedric Bosdonnat's patch from https://sourceforge.net/p/raspberry-gpio-python/tickets/149

--- wiringPi/wiringPi.c
+++ wiringPi/wiringPi.c
@@ -687,110 +687,58 @@ static void piGpioLayoutOops (const char *why)
 
 int piGpioLayout (void)
 {
-  FILE *cpuFd ;
-  char line [120] ;
-  char *c ;
+  FILE *dtcFd ;
+  char *line ;
   static int  gpioLayout = -1 ;
+  size_t linelen = 128;
 
   if (gpioLayout != -1)	// No point checking twice
     return gpioLayout ;
 
-  if ((cpuFd = fopen ("/proc/cpuinfo", "r")) == NULL)
-    piGpioLayoutOops ("Unable to open /proc/cpuinfo") ;
+  if ((dtcFd = fopen("/proc/device-tree/compatible", "r")) == NULL){
+    piGpioLayoutOops ("Unable to open /proc/device-tree/compatible") ;
+  }
 
 // Start by looking for the Architecture to make sure we're really running
 //	on a Pi. I'm getting fed-up with people whinging at me because
 //	they can't get it to work on weirdFruitPi boards...
 
-  while (fgets (line, 120, cpuFd) != NULL)
-    if (strncmp (line, "Hardware", 8) == 0)
-      break ;
+  line = calloc(linelen, sizeof(char));
 
-  if (strncmp (line, "Hardware", 8) != 0)
-    piGpioLayoutOops ("No \"Hardware\" line") ;
+  while (getdelim(&line, &linelen, '\0', dtcFd) != -1) {
 
-  if (wiringPiDebug)
-    printf ("piGpioLayout: Hardware: %s\n", line) ;
+    // Look for the board model
+    if ((strstr(line, "raspberrypi,model-a")) || (strstr(line, "raspberrypi,model-b")) || (strstr(line, "raspberrypi,model-b-rev2"))) {
+       gpioLayout = 1;
+    }
+    // Covers everything else from the B revision 2 to the B+, the Pi v2, v3, zero and CM's.
+    else if ((strstr(line, "raspberrypi,model-a-plus")) || (strstr(line, "raspberrypi,model-b-plus")) || 
+      (strstr(line, "raspberrypi,2-model-b")) || (strstr(line, "raspberrypi,3-model-b")) || 
+      (strstr(line, "raspberrypi,model-zero")) || (strstr(line, "raspberrypi,compute-module"))) {
 
-// See if it's BCM2708 or BCM2709 or the new BCM2835.
+       gpioLayout = 2;
+    }
+    // See if it's brcm,bcm2835 or brcm,bcm2836 or the new brcm,bcm2837.
 
-// OK. As of Kernel 4.8,  we have BCM2835 only, regardless of model.
-//	However I still want to check because it will trap the cheapskates and rip-
-//	off merchants who want to use wiringPi on non-Raspberry Pi platforms - which
-//	I do not support so don't email me your bleating whinges about anything
-//	other than a genuine Raspberry Pi.
+    // OK. As of Kernel 4.8,  we have BCM2835 only, regardless of model.
+    //  However I still want to check because it will trap the cheapskates and rip-
+    //  off merchants who want to use wiringPi on non-Raspberry Pi platforms - which
+    //  I do not support so don't email me your bleating whinges about anything
+    //  other than a genuine Raspberry Pi.
+    else if (! ((strstr (line, "brcm,bcm2835") || strstr (line, "brcm,bcm2836") || strstr (line, "brcm,bcm2837"))))
+    {
+      fprintf (stderr, "Unable to determine hardware version. I see: %s,\n", line) ;
+      fprintf (stderr, " - expecting brcm,bcm2835, brcm,bcm2836 or brcm,bcm2837.\n") ;
+      fprintf (stderr, "If this is a genuine Raspberry Pi then please report this\n") ;
+      fprintf (stderr, "to projects@drogon.net. If this is not a Raspberry Pi then you\n") ;
+      fprintf (stderr, "are on your own as wiringPi is designed to support the\n") ;
+      fprintf (stderr, "Raspberry Pi ONLY.\n") ;
+      exit (EXIT_FAILURE) ;
+    }
 
-  if (! (strstr (line, "BCM2708") || strstr (line, "BCM2709") || strstr (line, "BCM2835")))
-  {
-    fprintf (stderr, "Unable to determine hardware version. I see: %s,\n", line) ;
-    fprintf (stderr, " - expecting BCM2708, BCM2709 or BCM2835.\n") ;
-    fprintf (stderr, "If this is a genuine Raspberry Pi then please report this\n") ;
-    fprintf (stderr, "to projects@drogon.net. If this is not a Raspberry Pi then you\n") ;
-    fprintf (stderr, "are on your own as wiringPi is designed to support the\n") ;
-    fprintf (stderr, "Raspberry Pi ONLY.\n") ;
-    exit (EXIT_FAILURE) ;
+    // Right - we're Probably on a Raspberry Pi. Check the revision field for the real
+    // hardware type
   }
-
-// Right - we're Probably on a Raspberry Pi. Check the revision field for the real
-//	hardware type
-//	In-future, I ought to use the device tree as there are now Pi entries in
-//	/proc/device-tree/ ...
-//	but I'll leave that for the next revision.
-
-// Isolate the Revision line
-
-  rewind (cpuFd) ;
-  while (fgets (line, 120, cpuFd) != NULL)
-    if (strncmp (line, "Revision", 8) == 0)
-      break ;
-
-  fclose (cpuFd) ;
-
-  if (strncmp (line, "Revision", 8) != 0)
-    piGpioLayoutOops ("No \"Revision\" line") ;
-
-// Chomp trailing CR/NL
-
-  for (c = &line [strlen (line) - 1] ; (*c == '\n') || (*c == '\r') ; --c)
-    *c = 0 ;
-  
-  if (wiringPiDebug)
-    printf ("piGpioLayout: Revision string: %s\n", line) ;
-
-// Scan to the first character of the revision number
-
-  for (c = line ; *c ; ++c)
-    if (*c == ':')
-      break ;
-
-  if (*c != ':')
-    piGpioLayoutOops ("Bogus \"Revision\" line (no colon)") ;
-
-// Chomp spaces
-
-  ++c ;
-  while (isspace (*c))
-    ++c ;
-
-  if (!isxdigit (*c))
-    piGpioLayoutOops ("Bogus \"Revision\" line (no hex digit at start of revision)") ;
-
-// Make sure its long enough
-
-  if (strlen (c) < 4)
-    piGpioLayoutOops ("Bogus revision line (too small)") ;
-
-// Isolate  last 4 characters: (in-case of overvolting or new encoding scheme)
-
-  c = c + strlen (c) - 4 ;
-
-  if (wiringPiDebug)
-    printf ("piGpioLayout: last4Chars are: \"%s\"\n", c) ;
-
-  if ( (strcmp (c, "0002") == 0) || (strcmp (c, "0003") == 0))
-    gpioLayout = 1 ;
-  else
-    gpioLayout = 2 ;	// Covers everything else from the B revision 2 to the B+, the Pi v2, v3, zero and CM's.
 
   if (wiringPiDebug)
     printf ("piGpioLayoutOops: Returning revision: %d\n", gpioLayout) ;
