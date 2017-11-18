ce4c08fb722a5689267e5ee231500614bc569c58
blecher-at <token_github@blecher.at>
2017-11-18 10:29:12 +0000

    change owner of active_low as well on export.

--- gpio/gpio.c
+++ gpio/gpio.c
@@ -530,6 +530,8 @@ void doExport (int argc, char *argv [])
   sprintf (fName, "/sys/class/gpio/gpio%d/edge", pin) ;
   changeOwner (argv [0], fName) ;
 
+  sprintf (fName, "/sys/class/gpio/gpio%d/active_low", pin) ;
+  changeOwner (argv [0], fName) ;
 }
 
 
