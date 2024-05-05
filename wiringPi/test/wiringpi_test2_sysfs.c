fce0c49ae7162ab637706144d56809caa14b8799
mstroh76 <m.stroh76@gmail.com>
2024-05-05 14:43:13 +0200

    #234

--- wiringPi/test/wiringpi_test2_sysfs.c
+++ wiringPi/test/wiringpi_test2_sysfs.c
@@ -1,8 +1,7 @@
 ﻿// WiringPi test program: Kernel char device interface / sysfs successor
 // Compile: gcc -Wall wiringpi_test2_device.c -o wiringpi_test2_device -lwiringPi
 
-#include <wiringPi.h>
-#include <stdio.h>
+#include "wpi_test.h"
 #include <stdlib.h>
 #include <signal.h>
 #include <string.h>
@@ -12,33 +11,8 @@
 
 const int GPIO = 19;
 const int GPIOIN = 26;
-const int  ToggleValue = 4;
+const int ToggleValue = 4;
 
-void CheckGPIO(int out) {
-	int in  = digitalRead(GPIOIN);
-	int read = digitalRead(GPIO);
-
-	int pass = 0;
-	if (out==in && in==read) {
-		pass = 1;
-	}
-	printf("GPIO%d = %d (GPIO%d = %d)  -> %s\n", GPIOIN, in, GPIO, read, pass ? "passed":"failed" );
-}
-
-void digitalWriteEx(int pin, int mode) {
-	digitalWrite(pin, mode);
-	printf("out = %d   ", mode);
-	delayMicroseconds(5000);
-	CheckGPIO(mode);
-}
-
-void pullUpDnControlEx (int pin ,int mode) {
-	pullUpDnControl (pin, mode);
-	int out  = mode==PUD_UP ? 1:0;
-	printf("in = %4s   ", mode==PUD_UP ? "up":"down");
-	delayMicroseconds(5000);
-	CheckGPIO(out);
-}
 
 int main (void) {
 
@@ -53,19 +27,19 @@ int main (void) {
 	pinMode(GPIO, OUTPUT);
 
 	printf("\nTest output\n");
-	digitalWriteEx(GPIO, HIGH);
+	digitalWriteEx(GPIO, GPIOIN, HIGH);
 	delayMicroseconds(600000);
-	digitalWriteEx(GPIO, LOW);
+	digitalWriteEx(GPIO, GPIOIN, LOW);
 	delayMicroseconds(600000);
 
     printf("\nTest output off with pull up\n");
     pinMode(GPIO, OUTPUT);
-    digitalWriteEx(GPIO, LOW);
+    digitalWriteEx(GPIO, GPIOIN, LOW);
     pullUpDnControl (GPIO, PUD_UP);
     pinMode(GPIO, PM_OFF);
     delayMicroseconds(600000);
     printf("out = off ");
-    CheckGPIO(HIGH);
+    CheckGPIO(GPIO, GPIOIN, HIGH);
     delayMicroseconds(600000);
 
 	printf("\nTest output off with pull down\n");
@@ -73,7 +47,7 @@ int main (void) {
 	pinMode(GPIO, PM_OFF);
 	delayMicroseconds(600000);
 	printf("out = off ");
-	CheckGPIO(LOW);
+	CheckGPIO(GPIO, GPIOIN, LOW);
 	delayMicroseconds(600000);
 
 	return(EXIT_SUCCESS);
