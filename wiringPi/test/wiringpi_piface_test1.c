5399409d4ab86bfbe141257a119a573a7087ce2f
mstroh76 <m.stroh76@gmail.com>
2024-06-08 18:59:06 +0200

    #256 i2c to XO, prepare piface

---
+++ wiringPi/test/wiringpi_piface_test1.c
@@ -0,0 +1,17 @@
+﻿#include "wpi_test.h"
+#include "../../version.h"
+
+// PiFace dummy
+
+int main (void) {
+  int major, minor;
+
+
+  wiringPiVersion(&major, &minor);
+  printf("Testing piface functions with WiringPi %d.%d\n",major, minor);
+  printf("------------------------------------------\n\n");
+
+
+
+  return UnitTestState();
+}
