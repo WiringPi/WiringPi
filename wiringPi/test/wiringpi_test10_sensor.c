174e86b2fdc316a02e8160607d06f74546a38ee9
m.stroh76m <m.stroh76@gmail.com>
2025-02-14 16:33:46 +0100

    #314 - DS18B20

---
+++ wiringPi/test/wiringpi_test10_sensor.c
@@ -0,0 +1,23 @@
+﻿#include "wpi_test.h"
+#include <ds18b20.h> 
+#include <unistd.h>
+
+
+#define GROUND_T_PIN_BASE 100
+
+int main (void) {
+
+  printf("Load w1-gpio overlay at GPIO4\n");
+  system("sudo dtoverlay w1-gpio gpiopin=4 pullup=1");
+  sleep(2);
+
+  ds18b20Setup(GROUND_T_PIN_BASE, "011879ee7fff");
+
+  int value = analogRead(GROUND_T_PIN_BASE);
+  float fvalue = value/10.0f;
+  //printf("Temp.: %4.1f*C (%d)\n", fvalue, value);
+  CheckSameFloat("Temperature 18B20 Sensor at GPIO4", fvalue, 24.0f, 4.0f);
+  
+
+  return UnitTestState();
+}
