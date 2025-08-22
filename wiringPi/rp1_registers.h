7405ab0fdbbbaa05e3dfa0bb39afbda34c305cc3
Connor Gibson <connor@nextdoortech.net>
2025-08-21 17:02:01 -0700

    Remove copyright notice from new headers (per request)

--- wiringPi/rp1_registers.h
+++ wiringPi/rp1_registers.h
@@ -2,7 +2,6 @@
  * rp1_registers.h:
  *  Private header defining data structures used to interface directly
  *  with memory on RP1 Raspberry Pi GPIO hardware.
- *  Copyright (c) 2025 Grazer Computer Club and contributors
  ***********************************************************************
  * This file is part of wiringPi:
  *    https://github.com/WiringPi/WiringPi
