1d91e2b1d8984159f256b66079e62227f445eec0
Connor Gibson <connor@nextdoortech.net>
2025-09-05 13:53:43 -0700

    Fix missing padding in BCM_PWM_BANK

--- wiringPi/bcm_registers.h
+++ wiringPi/bcm_registers.h
@@ -33,8 +33,6 @@
 
 #define CHECK_OFFSET(struct_type, member, offset) static_assert(offsetof(struct_type, member) == offset, "Invalid offset for member " #member " of structure " #struct_type ". Should match " #offset ".");
 
-
-
 typedef struct [[gnu::packed]] BCM_PWM_BANK {
 
   // Note: The channel numbers used differ from the peripherals documentation for the BCM283X / BCM2711 chips.
@@ -42,40 +40,40 @@ typedef struct [[gnu::packed]] BCM_PWM_BANK {
   //    Structure members have been named using the WiringPi convention, but the Broadcom names are listed
   //    in parentheses in the comments.
 
-  union BCM_PWM_CTRL_REGISTER { // CTL : PWM Control; Offset 0x00
+  union BCM_PWM_CTRL_REGISTER {  // CTL : PWM Control; Offset 0x00
     volatile uint32_t CTRL_register;
     struct {
-      volatile uint32_t CHAN0_ENABLE    : 1;  // Channel 0 Enable (PWEN1)
-      volatile uint32_t CHAN0_MODE      : 1;  // Channel 0 Mode (MODE1)
-      volatile uint32_t CHAN0_REPEAT    : 1;  // Channel 0 Repeat Last Data (RPTL1)
-      volatile uint32_t CHAN0_SILENCE   : 1;  // Channel 0 Silence Bit (SBIT1)
-      volatile uint32_t CHAN0_POLARITY  : 1;  // Channel 0 Polarity (POLA1)
-      volatile uint32_t CHAN0_FIFO      : 1;  // Channel 0 Use FIFO (USEF1)
-      volatile uint32_t CLEAR_FIFO      : 1;  // Clear FIFO (CLRF)
-      volatile uint32_t CHAN0_MS_ENABLE : 1;  // Channel 0 M/S Enable (MSEN1)
-      volatile uint32_t CHAN1_ENABLE    : 1;  // Channel 1 Enable (PWEN2)
-      volatile uint32_t CHAN1_MODE      : 1;  // Channel 1 Mode (MODE2)
-      volatile uint32_t CHAN1_REPEAT    : 1;  // Channel 1 Repeat Last Data (RPTL2)
-      volatile uint32_t CHAN1_SILENCE   : 1;  // Channel 1 Silence Bit (SBIT2)
-      volatile uint32_t CHAN1_POLARITY  : 1;  // Channel 1 Polarity (POLA2)
-      volatile uint32_t CHAN1_FIFO      : 1;  // Channel 1 Use FIFO (USEF2)
-      volatile uint32_t                 : 1;  // Reserved
-      volatile uint32_t CHAN1_MS_ENABLE : 1;  // Channel 1 M/S Enable (MSEN2)
-      volatile uint32_t                 : 16; // Reserved
+      volatile uint32_t CHAN0_ENABLE    : 1;   // Channel 0 Enable (PWEN1)
+      volatile uint32_t CHAN0_MODE      : 1;   // Channel 0 Mode (MODE1)
+      volatile uint32_t CHAN0_REPEAT    : 1;   // Channel 0 Repeat Last Data (RPTL1)
+      volatile uint32_t CHAN0_SILENCE   : 1;   // Channel 0 Silence Bit (SBIT1)
+      volatile uint32_t CHAN0_POLARITY  : 1;   // Channel 0 Polarity (POLA1)
+      volatile uint32_t CHAN0_FIFO      : 1;   // Channel 0 Use FIFO (USEF1)
+      volatile uint32_t CLEAR_FIFO      : 1;   // Clear FIFO (CLRF)
+      volatile uint32_t CHAN0_MS_ENABLE : 1;   // Channel 0 M/S Enable (MSEN1)
+      volatile uint32_t CHAN1_ENABLE    : 1;   // Channel 1 Enable (PWEN2)
+      volatile uint32_t CHAN1_MODE      : 1;   // Channel 1 Mode (MODE2)
+      volatile uint32_t CHAN1_REPEAT    : 1;   // Channel 1 Repeat Last Data (RPTL2)
+      volatile uint32_t CHAN1_SILENCE   : 1;   // Channel 1 Silence Bit (SBIT2)
+      volatile uint32_t CHAN1_POLARITY  : 1;   // Channel 1 Polarity (POLA2)
+      volatile uint32_t CHAN1_FIFO      : 1;   // Channel 1 Use FIFO (USEF2)
+      volatile uint32_t                 : 1;   // Reserved
+      volatile uint32_t CHAN1_MS_ENABLE : 1;   // Channel 1 M/S Enable (MSEN2)
+      volatile uint32_t                 : 16;  // Reserved
     };
     struct BCM_PWM_CTRL_CHAN_FIELDS {
-      volatile uint8_t ENABLE     : 1;  // Enable (PWEN1/2)
-      volatile uint8_t MODE       : 1;  // Mode (MODE1/2)
-      volatile uint8_t REPEAT     : 1;  // Repeat Last Data (RPTL1/2)
-      volatile uint8_t SILENCE    : 1;  // Silence Bit (SBIT1/2)
-      volatile uint8_t POLARITY   : 1;  // Polarity (POLA1/2)
-      volatile uint8_t USE_FIFO   : 1;  // Use FIFO (USEF1/2)
-      volatile uint8_t            : 1;  // Reserved (really CLRF for CHAN[0], reserved for CHAN[1])
-      volatile uint8_t MS_ENABLE  : 1;  // M/S Enable (MSEN1/2)
+      volatile uint8_t ENABLE    : 1;  // Enable (PWEN1/2)
+      volatile uint8_t MODE      : 1;  // Mode (MODE1/2)
+      volatile uint8_t REPEAT    : 1;  // Repeat Last Data (RPTL1/2)
+      volatile uint8_t SILENCE   : 1;  // Silence Bit (SBIT1/2)
+      volatile uint8_t POLARITY  : 1;  // Polarity (POLA1/2)
+      volatile uint8_t USE_FIFO  : 1;  // Use FIFO (USEF1/2)
+      volatile uint8_t           : 1;  // Reserved (really CLRF for CHAN[0], reserved for CHAN[1])
+      volatile uint8_t MS_ENABLE : 1;  // M/S Enable (MSEN1/2)
     } CHAN[2];
   } CTRL;
 
-  union BCM_PWM_STATUS_REGISTER { // PWM Status (STA); Offset 0x04
+  union BCM_PWM_STATUS_REGISTER {  // PWM Status (STA); Offset 0x04
     volatile uint32_t STATUS_register;
     struct {
       const volatile uint32_t ERR_FULL    : 1;  // FIFO Full Flag (FULL1)
@@ -92,35 +90,36 @@ typedef struct [[gnu::packed]] BCM_PWM_BANK {
   } STATUS;
 
   union {
-    struct { // Individually named registers
-      union BCM_PWM_DMA_CONF { // PWM DMA Configuration (DMAC); Offset 0x08
+    struct {                    // Individually named registers
+      union BCM_PWM_DMA_CONF {  // PWM DMA Configuration (DMAC); Offset 0x08
         volatile uint32_t DMA_CONF_register;
         struct {
-          volatile uint32_t DREQ    : 8;  // DMA Threshold for DREQ signal
-          volatile uint32_t PANIC   : 8;  // DMA Threshold for PANIC signal
-          volatile uint32_t         : 15; // Reserved
-          volatile uint32_t ENABLE  : 1;  // DMA Enable
+          volatile uint32_t DREQ   : 8;   // DMA Threshold for DREQ signal
+          volatile uint32_t PANIC  : 8;   // DMA Threshold for PANIC signal
+          volatile uint32_t        : 15;  // Reserved
+          volatile uint32_t ENABLE : 1;   // DMA Enable
         };
       } DMA_CONF;
 
-      volatile uint32_t CHAN0_RANGE;  // Channel 0 Range (RNG1); Offset 0x10
-      volatile uint32_t CHAN0_DATA;   // Channel 0 Data (DAT1); Offset 0x14
-      volatile uint32_t FIFO_IN;      // Channel FIFO Input (FIF1); Offset 0x18
-      volatile uint32_t CHAN1_RANGE;  // Channel 1 Range (RNG2); Offset 0x20
-      volatile uint32_t CHAN1_DATA;   // Channel 1 Data (DAT2); Offset 0x24
+      volatile uint32_t : 32;  // Reserved: Padding; Offset 0x0C
 
+      volatile uint32_t : 32;  // Padding: Overlaps with CHAN[0].RANGE; Offset 0x10
+      volatile uint32_t : 32;  // Padding: Overlaps with CHAN[0].DATA; Offset 0x14
+
+      volatile uint32_t FIFO_IN;  // Channel FIFO Input (FIF1); Offset 0x18
+
+      volatile uint32_t : 32;  // Reserved: Padding; Offset 0x0C
+
+      volatile uint32_t : 32;  // Padding: Overlaps with CHAN[1].RANGE; Offset 0x20
+      volatile uint32_t : 32;  // Padding: Overlaps with CHAN[1].DATA; Offset 0x24
     };
 
-    struct { // Indexed Channel Registers
-
-      struct BCM_PWM_CHAN {
-        volatile uint32_t : 32;   // Padding (Overlaps with DMA_CONF / FIFO_IN)
-        volatile uint32_t RANGE;  // Channel Range (RNG1/2); Offset 0x10/0x20
-        volatile uint32_t DATA;   // Channel Data (DAT1/2); Offset 0x14/0x24
-      } CHAN[2];
-
-    };
-
+    struct BCM_PWM_CHAN {
+      volatile uint32_t : 32;   // Padding: (Overlaps with DMA_CONF / FIFO_IN); Offset 0x08/0x18
+      volatile uint32_t : 32;   // Reserved: Padding; Offset 0x0C/0x1C
+      volatile uint32_t RANGE;  // Channel Range (RNG1/2); Offset 0x10/0x20
+      volatile uint32_t DATA;   // Channel Data (DAT1/2); Offset 0x14/0x24
+    } CHAN[2];
   };
 
 } BCM_PWM_BANK;
@@ -130,13 +129,9 @@ CHECK_OFFSET(BCM_PWM_BANK, CTRL.CHAN[0],  0x00);
 CHECK_OFFSET(BCM_PWM_BANK, CTRL.CHAN[1],  0x01);
 CHECK_OFFSET(BCM_PWM_BANK, STATUS,        0x04);
 CHECK_OFFSET(BCM_PWM_BANK, DMA_CONF,      0x08);
-CHECK_OFFSET(BCM_PWM_BANK, CHAN0_RANGE,   0x10);
-CHECK_OFFSET(BCM_PWM_BANK, CHAN0_DATA,    0x14);
 CHECK_OFFSET(BCM_PWM_BANK, CHAN[0].RANGE, 0x10);
 CHECK_OFFSET(BCM_PWM_BANK, CHAN[0].DATA,  0x14);
 CHECK_OFFSET(BCM_PWM_BANK, FIFO_IN,       0x18);
-CHECK_OFFSET(BCM_PWM_BANK, CHAN1_RANGE,   0x20);
-CHECK_OFFSET(BCM_PWM_BANK, CHAN1_DATA,    0x24);
 CHECK_OFFSET(BCM_PWM_BANK, CHAN[1].RANGE, 0x20);
 CHECK_OFFSET(BCM_PWM_BANK, CHAN[1].DATA,  0x24);
 
