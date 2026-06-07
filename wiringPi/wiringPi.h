e74f9f43b9515bfe3e8197175e4bcb3ec778e06a
mstroh76 <m.stroh76@gmail.com>
2026-06-07 18:20:01 +0200

    #427 fix sample with deprecated 'waitforInterrupt' function call

--- wiringPi/wiringPi.h
+++ wiringPi/wiringPi.h
@@ -302,7 +302,12 @@ struct WPIWfiStatus {
     long long int timeStamp_us; // time stamp in microseconds
 };
 
-//extern int  waitForInterrupt    (int pin, int ms);  unknown if still working, disabled for V3.16, please contact developer via github
+//extern int  waitForInterrupt    (int pin, int ms);  disabled since V3.16
+// Legacy call 'waitForInterrupt' is no longer supported.
+// It depended on sysfs edge configuration "gpio edge 17 falling",
+// which has been removed in modern kernels.
+// Use 'waitForInterrupt2' instead and define the desired edge explicitly.
+// Note: 'waitForInterrupt2' also provides built-in debounce support,
 extern int  wiringPiISR         (int pin, int mode, void (*function)(void)) ;
 extern struct WPIWfiStatus  waitForInterrupt2(int pin, int edgeMode, int ms, unsigned long debounce_period_us) ;   // V3.16
 extern int  wiringPiISR2       (int pin, int edgeMode, void (*function)(struct WPIWfiStatus wfiStatus, void* userdata), unsigned long debounce_period_us, void* userdata) ;  // V3.16
